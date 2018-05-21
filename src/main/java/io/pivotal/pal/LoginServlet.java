a399fd62c5bf635d58d01fdb329c90019407f442
Fatima, Suroor <Suroor.Fatima3@T-Mobile.com>
2018-05-21 04:48:18 -0700

    test commit

--- src/main/java/io/pivotal/pal/LoginServlet.java
+++ src/main/java/io/pivotal/pal/LoginServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-
+// test
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
     private final String username = "palstudent@pivotal.io";
